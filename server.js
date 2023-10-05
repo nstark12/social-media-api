e4064e14d89a1c3d50a84c24f0ecdd58c94bc019
Marketing Director <nicolestark1103@gmail.com>
2023-10-05 10:31:56 -0500

    routes set up

--- server.js
+++ server.js
@@ -20,84 +20,86 @@ db.once('open', () => {
     })
 })
 
-// const seedThoughts = async () => {
-//     await Thought.deleteMany({})
+const seedThoughts = async () => {
+    await Thought.deleteMany({})
 
-//     const thoughtData = [
-//         {
-//             thoughtText: 'This is a thought',
-//             username: 'user1',
-//             reactions: [
-//                 { reactionBody: 'This is a reaction', username: 'user2' }
-//             ]
-//         },
-//         {
-//             thoughtText: 'This is another thought',
-//             username: 'user2',
-//             reactions: [
-//                 { reactionBody: 'This is a reaction', username: 'user3' }
-//             ]
-//         },
-//         {
-//             thoughtText: 'This is my thought',
-//             username: 'user3',
-//             reactions: [
-//                 { reactionBody: 'This is a reaction', username: 'user4' }
-//             ]
-//         },
-//         {
-//             thoughtText: 'This is a fianl thought',
-//             username: 'user4',
-//             reactions: [
-//                 { reactionBody: 'This is a reaction', username: 'user5' }
-//             ]
-//         }
-//     ]
+    const thoughtData = [
+        {
+            thoughtText: 'This is a thought',
+            username: 'user1',
+            reactions: [
+                { reactionBody: 'This is a reaction', username: 'user2' }
+            ]
+        },
+        {
+            thoughtText: 'This is another thought',
+            username: 'user2',
+            reactions: [
+                { reactionBody: 'This is a reaction', username: 'user3' }
+            ]
+        },
+        {
+            thoughtText: 'This is my thought',
+            username: 'user3',
+            reactions: [
+                { reactionBody: 'This is a reaction', username: 'user4' }
+            ]
+        },
+        {
+            thoughtText: 'This is a fianl thought',
+            username: 'user4',
+            reactions: [
+                { reactionBody: 'This is a reaction', username: 'user5' }
+            ]
+        }
+    ]
 
-//     await Thought.insertMany(thoughtData)
-// }
+    await Thought.insertMany(thoughtData)
+}
 
-// const seedUsers = async () => {
-//     await User.deleteMany({})
+const seedUsers = async () => {
+    await User.deleteMany({})
 
-//     const [firstThought, secondThought, thirdThought, fourthThought] = await Thought.find()
+    const [firstThought, secondThought, thirdThought, fourthThought] = await Thought.find()
 
-//     // const [firstUser, secondUser, thirdUser, fourthUser] = await User.find()
+    // const [firstUser, secondUser, thirdUser, fourthUser] = await User.find()
 
-//     const userData = [
-//         {
-//             username: 'user1',
-//             email: 'user1@email.com',
-//             thoughts: [firstThought._id]
-//         },
-//         {
-//             username: 'user2',
-//             email: 'user2@email.com',
-//             thoughts: [secondThought._id]
-//         },
-//         {
-//             username: 'user3',
-//             email: 'user3@email.com',
-//             thoughts: [thirdThought._id]
-//         },
-//         {
-//             username: 'user4',
-//             email: 'user4@email.com',
-//             thoughts: [fourthThought._id]
-//         },
-//         {
-//             username: 'user5',
-//             email: 'user5@email.com'
-//         }
-//     ]
+    const userData = [
+        {
+            username: 'user1',
+            email: 'user1@email.com',
+            thoughts: [firstThought._id]
+        },
+        {
+            username: 'user2',
+            email: 'user2@email.com',
+            thoughts: [secondThought._id]
+        },
+        {
+            username: 'user3',
+            email: 'user3@email.com',
+            thoughts: [thirdThought._id]
+        },
+        {
+            username: 'user4',
+            email: 'user4@email.com',
+            thoughts: [fourthThought._id]
+        },
+        {
+            username: 'user5',
+            email: 'user5@email.com'
+        }
+    ]
 
-//     await User.insertMany(userData)
-// }
+    await User.insertMany(userData)
+}
 
-// const init = async () => {
-//     // const users = await User.find({})
-//     await seedThoughts()
-//     await seedUsers()
-// }
+const init = async () => {
+    
+    await seedThoughts()
+    await seedUsers()
+    const users = await User.find({}).populate('thoughts')
+    console.log(JSON.stringify(users, null, 4))
+}
 
-// init()
+init()
