7669ea9b881a3358cd00bf62784e8718855c1f36
Marketing Director <nicolestark1103@gmail.com>
2023-10-05 10:09:04 -0500

    seeded data

--- models/Thought.js
+++ models/Thought.js
@@ -1,23 +1,24 @@
+const dayjs = require('dayjs')
+
 const { Schema, model } = require('mongoose')
 const reactionSchema = require('./Reaction')
 const formatDate = require('../utils/formatDate')
 
-const thoughtSchema = new Schemal ({
+const thoughtSchema = new Schema ({
     thoughtText: {
         type: String,
         required: true,
         minlength: 1,
         maxlength: 280
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        //TODO: getter to format date
-        // get: (timestamp) => formatDate(timestamp)
-    },
     username: {
         type: String,
         required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dayjs(timestamp).format('dddd, MMMM D')
     },
     reactions: [reactionSchema],
 }, {
---
+++ utils/data.js
@@ -0,0 +1,12 @@
+const usernames = [
+    'user1',
+    'user2',
+    'user3',
+    'user4',
+    'user5',
+    'user6',
+    'user7',
+    'user8',
+    'user9',
+    'user10'
+]
